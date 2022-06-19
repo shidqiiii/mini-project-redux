272f97385d5b71f1b10370d53575e7d3f6867155
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-19 21:25:25 +0700

    Update ShoppingCartPage.js

--- src/Pages/ShoppingCartPage.js
+++ src/Pages/ShoppingCartPage.js
@@ -32,27 +32,26 @@ function ShoppingCartPage(props) {
     }, [props.cartReducer]);
 
     return (
-        <Container>
+        <Container className='mt-5'>
             {listCart.map((e) => (
-                <Card className="mt-5 px-4" key={e.product_id}>
+                <Card className="mt-3 px-4" key={e.product_id}>
                     <div className="row g-0">
                         <div className="col-md-2">
                             <Image src={e.image} className="img-fluid rounded-start" alt="image content" />
                         </div>
-                        <div className="col-md d-flex align-items-center">
+                        <div className="col-md-7 d-flex align-items-center">
                             <Card.Body>
                                 <Card.Title>{e.product_name}</Card.Title>
-                                <Card.Text>{e.total_price}</Card.Text>
+                                <Card.Text>IDR {e.price.toLocaleString("id-ID")}</Card.Text>
                             </Card.Body>
                         </div>
-                        <div className="col-md-2 d-flex align-items-center justify-content-center">
+                        <div className="col-md-3 d-flex align-items-center justify-content-center">
                             <Button variant='dark'>-</Button>
                             <Card.Text className='my-auto mx-3'>{e.total_item}</Card.Text>
                             <Button variant='dark'>+</Button>
+                            <Card.Text className='my-auto mx-3'>IDR {e.total_price.toLocaleString("id-ID")}</Card.Text>
                         </div>
-                        <div className="col-md-1 d-flex align-items-center justify-content-center mx-3">
-                            <Button variant='dark' className='my-auto' onClick={() => { props.dispatch(deleteCart(e.product_id)) }}>Hapus</Button>
-                        </div>
+                        <Button variant='dark' className='ms-auto mb-3' style={{ width: "100px" }} onClick={() => { props.dispatch(deleteCart(e.product_id)) }}>Hapus</Button>
                     </div>
                 </Card>
             ))}
