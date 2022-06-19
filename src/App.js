232debe8c7067dfda931ac6cd8e9885ccb3e5f61
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-19 13:20:20 +0700

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
 import Home from './Pages/HomePage'
 import Product from './Pages/ProductPage'
 import ShoppingCart from './Pages/ShoppingCartPage'
@@ -8,18 +9,14 @@ import ShoppingCart from './Pages/ShoppingCartPage'
 function App() {
   return (
     <>
-      <div>
-        <Router>
-          <Link to="/">Home</Link>
-          <Link to="/product">Product</Link>
-          <Link to="/chart">Chart</Link>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/chart" element={<ShoppingCart />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/chart" element={<ShoppingCart />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+export default function NavigationBar() {
+    return (
+        <Navbar bg="dark" variant='dark' expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Ipaid!</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/product">Laptop</Nav.Link>
+                        <Nav.Link as={Link} to="/product">Smartphone</Nav.Link>
+                        <Nav.Link as={Link} to="/product">Personal Computer</Nav.Link>
+                    </Nav>
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/chart">Chart</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
