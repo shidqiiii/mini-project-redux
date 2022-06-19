aeadaff9dcdefd8df7f94c10312fa0b348559b1d
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-19 14:32:07 +0700

    create home page

---
+++ src/Components/Homepage/data.js
@@ -0,0 +1,30 @@
+const carouselItem = [{
+    "src": "carousel1.png",
+    "alt": "First slide"
+}, {
+    "src": "carousel2.png",
+    "alt": "Second slide"
+}, {
+    "src": "carousel3.png",
+    "alt": "Third slide"
+}]
+
+const cardItem = [{
+    image: "https://els.id/media/catalog/product/cache/31f3c0b07cbf4027df3ecbd24b5c34ea/h/p/hp_245_g7.jpg",
+    price: 5999000,
+    name: "HP 245-G8 - Black [Ryzen 3 5300U-4GB-SSD 256GB]"
+}, {
+    image: "https://els.id/media/catalog/product/cache/31f3c0b07cbf4027df3ecbd24b5c34ea/m/o/modern_14-b10mw_grey_5__3.jpg",
+    price: 10499000,
+    name: "MSI Modern 14-B5M-068ID - Carbon Grey [Ryzen 7 5700U-8GB-SSD 512GB] Slim Design"
+}, {
+    image: "https://els.id/media/catalog/product/cache/31f3c0b07cbf4027df3ecbd24b5c34ea/a/s/asus_creator_k3500_2.jpg",
+    price: 17659000,
+    name: "Asus Creator M3500QC-OLED955 - Cool Silver [Ryzen 9 5900H-16GB-SSD 512GB-RTX3050]"
+}, {
+    image: "https://els.id/media/catalog/product/cache/31f3c0b07cbf4027df3ecbd24b5c34ea/a/l/all_in_one_acer_c22-1650_usb_2.jpg",
+    price: 7529000,
+    name: "PC All In One Acer C22-1650 (i3) (LED 22' FHD) HDD"
+}]
+
+export { carouselItem, cardItem }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,37 @@
+import { Carousel, Container, Row, Col, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import React from 'react'
+import '../css/Homepage.css';
+import { carouselItem, cardItem } from "../Components/Homepage/data"
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <Container>
+            <Carousel className='mt-5'>
+                {carouselItem.map((e) => (
+                    <Carousel.Item interval={2000}>
+                        <img
+                            className="d-block h-100 w-100"
+                            src={"./Images/Carousel/" + e.src}
+                            alt={e.alt}
+                        />
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+
+            <Row xs={1} md={2} lg={4} className="g-4 my-5 text-center">
+                {cardItem.map((e) => (
+                    <Col>
+                        <Card style={{ minHeight: "390px" }} as={Link} to="/product">
+                            <Card.Img variant="top" src={e.image} />
+                            <Card.Body>
+                                <Card.Title>IDR {e.price.toLocaleString("id-ID")}</Card.Title>
+                                <Card.Text>{e.name}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
